5e9b44d929eebdfe93b1ffdc92e445264c09e174
Daniel Ordor <danielordor@rocketmail.com>
2018-03-26 20:27:30 +0100

    Initial Commit

---
+++ Daniel_RacingGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daniel_RacingGame
+//
+//  Created by Daniel Ordor on 26/03/2018.
+//  Copyright © 2018 Daniel Ordor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
